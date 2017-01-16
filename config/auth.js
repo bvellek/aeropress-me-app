7f817b4cef737c622b274443882e1dd5f88189c6
bvellek <bvellek@uw.edu>
2017-01-15 19:13:12 -0800

    changed google oauth callback

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,7 @@ module.exports = {
   'googleAuth' : {
     'clientID' : '509442669432-udflthcvtlks0jtm182uko7045o9mf9a.apps.googleusercontent.com',
     'clientSecret' : 'cTVxBq3wnmCVGrYt0MPOBvoL',
-    'callbackURL' : 'http://localhost:8080/auth/google/callback'
+    // 'callbackURL' : 'http://localhost:8080/auth/google/callback'
+    'callbackURL' : 'https://aeropress-me-app.herokuapp.com/auth/google/callback'
   }
 }
