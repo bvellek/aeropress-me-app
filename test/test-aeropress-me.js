81e11415a9ad5735377b767b365dca562bd1f0e5
bvellek <bvellek@uw.edu>
2017-01-17 17:21:27 -0800

    add test for submit new recipe page

--- test/test-aeropress-me.js
+++ test/test-aeropress-me.js
@@ -96,11 +96,7 @@ describe('Render Pages', function() {
   before(function() {
     return runServer().then((port) => {
       Browser.localhost('localhost', port);
-      this.browser = new Browser(
-      //   {
-      //   site: `http://localhost:${port}`
-      // }
-    );
+      this.browser = new Browser();
     });
   });
 
@@ -133,20 +129,6 @@ describe('Render Pages', function() {
       this.browser.visit('/login', done);
     });
 
-    it('should show HTML', () => {
-      const resolvingPromise = new Promise((resolve, reject) => {
-        chai.request(app)
-          .get('/login')
-          .end(function(err, res) {
-            res.should.have.status(200);
-            res.should.be.html;
-            resolve('it resolved');
-          });
-      });
-      return resolvingPromise.then((result) => {
-        expect(result).to.equal('it resolved');
-      });
-    });
 
     it('should show a login form', function() {
       assert.ok(this.browser.success);
@@ -181,25 +163,11 @@ describe('Render Pages', function() {
 
 
   describe('Registration Page', () => {
-    // load the Registration page
+    // Load the Registration page
     before(function(done) {
       this.browser.visit('/registration', done);
     });
 
-    it('should show HTML', () => {
-      const resolvingPromise = new Promise((resolve, reject) => {
-        chai.request(app)
-          .get('/registration')
-          .end(function(err, res) {
-            res.should.have.status(200);
-            res.should.be.html;
-            resolve('it resolved');
-          });
-      });
-      return resolvingPromise.then((result) => {
-        expect(result).to.equal('it resolved');
-      });
-    });
 
     it('should show a registration form', function() {
       assert.ok(this.browser.success);
@@ -241,7 +209,7 @@ describe('Render Pages', function() {
   })
 
   describe('My Recipes Page', () => {
-    // load the login page
+    // Load the login page & sign in with testuser@aeropressme.com
     before(function(done) {
       this.browser.visit('/login')
         .then(() => {
@@ -254,53 +222,94 @@ describe('Render Pages', function() {
         .then(done, done);
     });
 
-
-    it('should show My Recipes Page', function() {
-
-      const resolvingPromise = new Promise((resolve, reject) => {
-        chai.request(app)
-          .get('/myrecipes')
-          .end(function(err, res) {
-            res.should.have.status(200);
-            res.should.be.html;
-            resolve('it resolved');
-          });
-      });
-      return resolvingPromise.then((result) => {
-        console.log(result);
-        expect(result).to.equal('it resolved');
-      });
+    after(function(done) {
+      this.browser.visit('/login')
+      .then(done, done);
     });
+
 
     it('should show a "my recipes" header', function() {
       assert.ok(this.browser.success);
       assert.equal(this.browser.text('.your-recipes-page h2'), 'My Recipes');
     });
 
-    // it('should have email input and password input with a method of post for login form', function() {
-    //   this.browser.assert.element('#user-email');
-    //   this.browser.assert.element('#password');
-    //   this.browser.assert.attribute('form', 'method', 'post');
-    // });
-    //
-    // it('should show fail message for incorrect username', function(done) {
-    //   this.browser.fill('#user-email', 'notauser@mail.com');
-    //   this.browser.fill('#password', 'incorrectpassword');
-    //   this.browser.pressButton('button').then(() => {
-    //     assert.ok(this.browser.success);
-    //     assert.equal(this.browser.text('section.flash-alert'), 'No user found.');
-    //   }).then(done, done);
-    // });
-    //
-    // it('should show fail message for incorrect password', function(done) {
-    //   this.browser.fill('#user-email', 'testuser@aeropressme.com');
-    //   this.browser.fill('#password', 'incorrectpassword');
-    //   this.browser.pressButton('button').then(() => {
-    //     assert.ok(this.browser.success);
-    //     assert.equal(this.browser.text('section.flash-alert'), 'Oops! Password is incorrect.');
-    //   }).then(done, done);
-    // });
   })
+
+  describe('Submit New Recipe Page', () => {
+    // Load the login page & sign in with testuser@aeropressme.com & visit New Recipe Page
+    before(function(done) {
+      this.browser.visit('/login')
+        .then(() => {
+          this.browser.fill('#user-email', 'testuser@aeropressme.com');
+          this.browser.fill('#password', 'password');
+        })
+        .then(() => {
+          return this.browser.pressButton('button');
+        })
+        .then(() => {
+          return this.browser.visit('/newrecipe');
+        })
+        .then(done, done);
+    });
+
+    after(function(done) {
+      this.browser.visit('/login')
+      .then(done, done);
+    });
+
+
+    it('should show a "New AeroPress Recipe" header', function() {
+      assert.ok(this.browser.success);
+      assert.equal(this.browser.text('.input-page h2'), 'New AeroPressMe Recipe');
+    });
+
+    it('should have a "New Recipe" form with method post and appropriate form inputs', function() {
+      this.browser.assert.attribute('form', 'method', 'post');
+      this.browser.assert.element('#input-title');
+      this.browser.assert.element('#input-author');
+      this.browser.assert.element('#standard');
+      this.browser.assert.element('#inverted');
+      this.browser.assert.element('#water');
+      this.browser.assert.element('#coffee');
+      this.browser.assert.element('#water-temp');
+      this.browser.assert.element('#grind-select');
+      this.browser.assert.element('#instructions');
+    })
+
+  })
+
+
+  describe('All Recipes Page', () => {
+    // Load the login page & sign in with testuser@aeropressme.com & visit All recipes page
+    before(function(done) {
+      this.browser.visit('/login')
+        .then(() => {
+          this.browser.fill('#user-email', 'testuser@aeropressme.com');
+          this.browser.fill('#password', 'password');
+        })
+        .then(() => {
+          return this.browser.pressButton('button');
+        })
+        .then(() => {
+          return this.browser.visit('/allrecipes');
+        })
+        .then(done, done);
+    });
+
+    after(function(done) {
+      this.browser.visit('/login')
+      .then(done, done);
+    });
+
+
+    it('should show a "Top Recipes" header', function() {
+      assert.ok(this.browser.success);
+      assert.equal(this.browser.text('.recipes-page h2'), 'Top Recipes');
+    });
+
+  })
+
+
 
 
 });
