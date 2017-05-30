5a08c474f6acc371dd83b6372ac4f8142d458f87
bvellek <bvellek@uw.edu>
2017-05-30 11:31:52 -0700

    change https redirect

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   let sslUrl;
 
   if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
-    sslUrl = ['https://aeropress-me-app.herokuapp.com/', req.url].join('');
+    sslUrl = ['https://aeropress-me-app.herokuapp.com', req.url].join('');
     return res.redirect(sslUrl);
   }
   return next();
